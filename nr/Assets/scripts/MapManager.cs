d69a276e3d8aac0ee5e5c44767e70fbc91101c7a
Alexander <alexstarivanov.2011@gmail.com>
2025-05-31 21:16:23 +0300

    Uachinou1
    added first room1 but it is not work now

---
+++ nr/Assets/scripts/MapManager.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MapManager : MonoBehaviour
+{
+    public List<room> Rooms = new List<room>();
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ nr/Assets/scripts/room.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class room : MonoBehaviour
+{
+    public Vector3 StartLocalPosition;
+    public Vector3 EndLocalPosition;
+    public Quaternion NormalRotation;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
